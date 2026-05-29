45dacbb7848fb84d6c896e9f1071ffcc30156265
svk369 <watchsvk369@gmail.com>
2026-05-29 21:31:01 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+
+<JavaScript>
+
+const CACHE_NAME = 'svk-care-cache-v1';
+const urlsToCache = [
+  '/',
+  '/?utm_source=pwa',
+  '/search'
+];
+
+// ដំឡើង Service Worker និងរក្សាទុក Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ទាញយកទិន្នន័យពី Cache ពេលគ្មានអ៊ីនធឺណិត
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+</JavaScript>
